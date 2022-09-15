292a535d36dffb388788f2b367fff8fc64e1c072
javier-ddom <112418641+javier-ddom@users.noreply.github.com>
2022-09-15 12:12:18 -0500

    Add files via upload

---
+++ SELECT exercises.sql
@@ -0,0 +1,19 @@
+use albums_db;
+describe albums;
+#select release_date from albums order by release_date;
+#rows 6, 23 artist, id is primary, 1967 and 2011
+#select name from albums where artist = "Pink Floyd"
+#DSOTM and the wall
+#select release_date from albums where name LIKE "%heart%"
+#1967
+
+#select genre from albums where name = 'Nevermind'
+#Grunge, Alternative rock
+
+#select name from albums where release_date LIKE '199%';
+
+#select name from albums where sales < 20000000
+
+#select name from albums where genre = 'Rock'
+#the query has to match
+
