8e0e8beb7492574816424e4fc902f91608cbdc54
javier-ddom <112418641+javier-ddom@users.noreply.github.com>
2022-09-15 13:06:28 -0500

    Add files via upload
    tables

---
+++ tables.sql
@@ -0,0 +1,16 @@
+use employees;
+show tables;
+describe employees; #int date varchart enum 
+#Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+#emp no, birthdate, hiredate
+
+#Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+#first and last name, gender
+
+#Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+#birthdate  hiredate
+
+#What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+#the empno
+
+show create table dept_manager
